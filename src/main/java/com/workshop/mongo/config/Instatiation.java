0d09d233c29e326361ab0f8758ff2f9f3875d16d
RaifePaiva <raife.tec@gmail.com>
2020-03-19 00:47:01 -0300

    Padrao DTO

--- src/main/java/com/workshop/mongo/config/Instatiation.java
+++ src/main/java/com/workshop/mongo/config/Instatiation.java
@@ -3,7 +3,6 @@ package com.workshop.mongo.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
